5e6c14a415756b30b8b69bd54d306c2594eaaea1
muraenok <arumkone@wrdf.org>
2014-06-19 16:31:04 -0700

    cleaned up directory moved code to folders

--- Mortgage amortization tool with gui.R
+++
@@ -1,7 +0,0 @@
-# mortgate amortization tool
-# http://www.r-chart.com/2010/11/mortgage-calculator-and-amortization.html
-
-library(fgui)
-gui(mortgage)
-
-mortgage(P=100000, I=10, L=7, amort=T, plotData=T)
